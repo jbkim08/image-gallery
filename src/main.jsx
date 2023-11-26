1a6f959204c02cc86cb0c603bc3c96970f73c9c1
jbkim08 <jbkim08@gmail.com>
2023-11-27 05:41:57 +0900

    컨텍스트 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { AppProvider } from './context.jsx';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<AppProvider>
+		<App />
+	</AppProvider>
+);
