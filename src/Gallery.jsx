4bb0543bb29e53e0436f6a94d0fdb9503d885996
jbkim08 <jbkim08@gmail.com>
2023-11-27 18:31:49 +0900

    언스플래시 데이터 가져오기

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,42 @@
+import { useEffect } from 'react';
+import { useGlobalContext } from './context';
+import { useFetch } from './useFetch';
+
+const url = `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`;
+
 const Gallery = () => {
-	return <div>Gallery</div>;
+	const { searchTerm } = useGlobalContext();
+	const { data, error, isPending } = useFetch(`${url}&query=${searchTerm}&per_page=20`);
+	console.log(data);
+
+	if (isPending) {
+		return (
+			<section className='image-container'>
+				<h4>Loading...</h4>
+			</section>
+		);
+	}
+	if (error) {
+		return (
+			<section className='image-container'>
+				<h4>{error}</h4>
+			</section>
+		);
+	}
+	return (
+		<section className='gallery'>
+			<ul className='images'>
+				{data &&
+					data.results.map((item) => {
+						const url = item?.urls?.regular;
+						return (
+							<li className='img' key={item.id}>
+								<img src={url} alt={item.alt_description}></img>
+							</li>
+						);
+					})}
+			</ul>
+		</section>
+	);
 };
 export default Gallery;
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -10,7 +10,7 @@ const SearchForm = () => {
 	};
 	return (
 		<section>
-			<h1 className='title'>unsplash images</h1>
+			<h1 className='title'>Image Gallery</h1>
 			<form className='search-form' onSubmit={handleSubmit}>
 				<input type='text' className='form-input search-input' name='search' placeholder='cat' />
 				<button type='submit' className='btn'>
